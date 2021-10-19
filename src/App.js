104374657ba8c7742e8b500fe5ce155b64af7d45
1724656923@qq.com <1724656923@qq.com>
2021-10-19 15:47:16 +0800

    git test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App" onClick={() => setCount(count + 1)}>
       { count }
       { [1, 2, 3, 4].map((ele, index) => 
-        <div style={{ margin: '10px' }} key={index} onClick={(ele, index) => {clickBtn(ele, index)}}>{ele}</div>
+        <div style={{ margin: '10px' }} key={index} onClick={(ele, index) => {clickBtn(ele, index)}}>{ele}----</div>
       )}
     </div>
   );
